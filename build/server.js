10b1fa5bf5bc2eeb37d9618fb0d685d46ad94b32
Zeztron <pateltharsh@gmail.com>
2019-06-18 21:45:41 -0400

    first commit

--- build/server.js
+++ build/server.js
@@ -4,12 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var data_1 = require("./data/data");
 var app = express_1.default();
+// console.log(JSON.parse(JSON.stringify(DataStore.tours)));
 app.get("/", function (req, res, next) {
     res.send("Tour Booking API");
 });
 app.get("/tours", function (req, res, next) {
-    res.send("Get a list of tours...");
+    res.json(data_1.DataStore.tours);
 });
 app.post("/tours", function (req, res, next) {
     res.send("Add a new tour...");
